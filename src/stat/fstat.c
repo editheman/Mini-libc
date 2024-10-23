45bfb0931265c3c2bd5a32a85f982f252aadc75b
Eduard-Stefan Pana <edipana0409@gmail.com>
2024-10-23 14:44:29 +0000

    Completing some stat files.

--- src/stat/fstat.c
+++ src/stat/fstat.c
@@ -7,5 +7,11 @@
 int fstat(int fd, struct stat *st)
 {
 	/* TODO: Implement fstat(). */
-	return -1;
+	int ret = syscall(__NR_fstat, fd, st);
+
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+	return ret;
 }
--- src/stat/stat.c
+++ src/stat/stat.c
@@ -8,5 +8,11 @@
 int stat(const char *restrict path, struct stat *restrict buf)
 {
 	/* TODO: Implement stat(). */
-	return -1;
+	int ret = syscall(__NR_stat, path, buf);
+
+	if (ret < 0) {
+		errno = -ret;
+		return -1;
+	}
+	return ret;
 }
